ecc78cbb6ba2c61994f6c33686e7b98b08f01cb8
Ojas-Poswal <ojasposwal@gmail.com>
2026-07-15 17:32:06 +0530

    add e-commerce project

---
+++ personal 04 js projects/03_E_Commerce/script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded',() => {
+    const products = [
+        {id : 1, name : "Product 1", price : 29.99},
+        {id : 2, name : "Product 2", price : 19.99},
+        {id : 3, name : "Product 3", price : 59.99}
+    ]
+
+    const cart = []
+
+    const productList = document.getElementById("product-list")
+    const cartItems = document.getElementById("cart-items")
+    const emptyCartMessage = document.getElementById("empty-cart")
+    const cartTotal = document.getElementById("cart-total")
+    const totalPriceDisplay = document.getElementById("total-price")
+    const checkOutBtn = Document.getElementById("checkout-btn")
+
+})
